0479fef26bafe1632611b9261dfaf832bdb705f2
Denis Rudolf <d.rudolf_private@gmx.de>
2019-12-03 00:28:21 +0100

    changed prepare_signal

--- CurveToSignalApp/views.py
+++ CurveToSignalApp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from . import signal
+import numpy as np
 
 def index(request):
     template = loader.get_template('CurveToSignalApp/index.html')
@@ -10,10 +11,10 @@ def index(request):
 def get_points(request):
     asx = request.POST.getlist('x[]')
     asy = request.POST.getlist('y[]')
-    dX,dY,adYCalc = prepare_signal([asx, asy])
-    data = {'X'    : dX.tolist(),
-            'Y'    : dY.tolist(),
-            #'YCalc': adYCalc
+    dX, dY, adYCalc_real = prepare_signal([asx, asy])
+    data = {'adX'           : dX.tolist(),
+            'adY'           : dY.tolist(),
+            'adYCalc_real': adYCalc_real.tolist()
             }    
     return JsonResponse(data)
 
@@ -25,6 +26,7 @@ def prepare_signal(points):
         ady = [float(y) for y in asy]
     oSignal = signal.Signal([adx,ady])
     #acFourierCoeff = oSignal.get_Fourier_coefficients()
-    adYCalc = oSignal.calculate_signal_from_Fourier_coeff()
+    adYCalc      = oSignal.calculate_signal_from_Fourier_coeff()
+    adYCalc_real = np.real(adYCalc)
     dX,dY = oSignal.dX, oSignal.dY
-    return dX,dY,adYCalc  
+    return dX,dY,adYCalc_real  
